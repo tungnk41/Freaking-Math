798fc6ec990bba5d78f078b6351e72c6579ee14f
Tung Nguyen <tung.nk41@gmail.com>
2020-08-30 20:52:01 +0700

    Start project Freaking math

---
+++ app/src/main/java/com/example/freakingmath/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.freakingmath
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
